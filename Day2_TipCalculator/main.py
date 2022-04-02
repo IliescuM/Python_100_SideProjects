c03ad9f6ad2433c8346a5bc071f91cf7433534d9
Iliescu Mihnea <62083449+IliescuM@users.noreply.github.com>
2022-04-02 16:49:17 +0300

    Update main.py
    Code refactoring

--- Day2_TipCalculator/main.py
+++ Day2_TipCalculator/main.py
@@ -2,10 +2,11 @@ import string
 
 
 print("``` Welcome to the tip calculator ```\n")
+
 total_bill = input("~ What was the total bill? $")
 number_people = input("~ How many people to split the bill? ")
-
 percentage_tip = input("~ What percentage tip would you like to give? 10, 12, or 15? ")
+
 if not (
     int(percentage_tip) == 10 or int(percentage_tip) == 12 or int(percentage_tip) == 15
 ):
@@ -22,10 +23,8 @@ if not (
             )
         break
 
+tip_formula = float(total_bill) / float(number_people) * (float(percentage_tip) / 100)
+result_pay = float(total_bill) / float(number_people) + tip_formula
+result_pay = round(result_pay, 1)
 
-result = float(total_bill) / float(number_people) + float(total_bill) / float(
-    number_people
-) * (float(percentage_tip) / 100)
-result = round(result, 1)
-
-print("~ Each person should pay: $" + str(result))
+print("~ Each person should pay: $" + str(result_pay))
