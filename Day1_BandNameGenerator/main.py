c019c40b94c538d09d816506654089ef58ab7220
Iliescu Mihnea <62083449+IliescuM@users.noreply.github.com>
2022-04-02 16:12:35 +0300

    Update main.py

--- Day1_BandNameGenerator/main.py
+++ Day1_BandNameGenerator/main.py
@@ -1,3 +1,4 @@
-city = input("Insert name of your home town: ")
-pet = input("Insert name of your pet: ")
+print("Welcome to the Band Generator")
+city = input("Insert name of your home town: \n")
+pet = input("Insert name of your pet: \n")
 print("Band name could be : ", city + " " + pet)
